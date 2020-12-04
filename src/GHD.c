0702a74d613272027f9b39cb8cdded66a1c07cd7
Cristina Tortora <grikris1@gmail.com>
2020-12-04 04:40:02 +0000

    version 2.3.4

--- src/GHD.c
+++ src/GHD.c
@@ -12,7 +12,11 @@
 #include <R_ext/BLAS.h>
 #endif
 #define COMMENTS 0 
-
+extern void dgemm_(const char *transa, const char *transb,
+const int *m, const int *n, const int *k,
+const double *alpha, const double *a,
+const int *lda, const double *b, const int *ldb,
+const double *beta, double *c, const int *ldc);
 
 
   
